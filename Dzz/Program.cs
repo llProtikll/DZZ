58d4918280388def9d43cdc59f38dc21e4c341fd
llProtikll <dimacornuta@gmail.com>
2025-10-04 22:51:23 +0300

    dz5

--- Dzz/Program.cs
+++ Dzz/Program.cs
@@ -1,242 +1,106 @@
-﻿namespace Dzz
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Dzz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("введите сумму выдачи от 1 до 9999");
 
-            int sum = int.Parse(Console.ReadLine());
+            //1
 
-            bool mistake;
-            if (sum < 1 || sum > 9999)
+            Console.WriteLine("задание 1:");
+            int number;
+            while (true)
             {
-                mistake = false;
+                Console.Write("введите целое четырехзначное число: ");
+                if (!int.TryParse(Console.ReadLine(), out number) || number < 1000 || number > 9999)
+                {
+                    Console.WriteLine("ошибка | число не четырехзначное или не целое");
+                }
+                else
+                {
+                    break;
+                }
             }
-            else
+            int count = 0;
+            for (int i = number; i > 0; i /= 10)
             {
-                mistake = true;
+                if ((i % 10) % 2 == 0)
+                {
+                    count++;
+                }
             }
-
-
-            string thousand;
-            if (sum / 1000 == 1)
-            {
-                thousand = "тысяча";
-            }
-            else if (sum / 1000 == 2)
-            {
-                thousand = "две тысячи";
-            }
-            else if (sum / 1000 == 3)
-            {
-                thousand = "три тысячи";
-            }
-            else if (sum / 1000 == 4)
-            {
-                thousand = "четыре тысячи";
-            }
-            else if (sum / 1000 == 5)
-            {
-                thousand = "пять тысяч";
-            }
-            else if (sum / 1000 == 6)
-            {
-                thousand = "шесть тысяч";
-            }
-            else if (sum / 1000 == 7)
-            {
-                thousand = "семь тысяч";
-            }
-            else if (sum / 1000 == 8)
-            {
-                thousand = "восемь тысяч";
-            }
-            else if (sum / 1000 == 9)
-            {
-                thousand = "девять тысяч";
-            }
-            else
-            {
-                thousand = "";
-            }
-
-
-            int sum2 = sum % 1000;
-            string hundred;
-            if (sum2 / 100 == 1)
-            {
-                hundred = "сто";
-            }
-            else if (sum2 / 100 == 2)
-            {
-                hundred = "двести";
-            }
-            else if (sum2 / 100 == 3)
-            {
-                hundred = "триста";
-            }
-            else if (sum2 / 100 == 4)
-            {
-                hundred = "четыреста";
-            }
-            else if (sum2 / 100 == 5)
-            {
-                hundred = "пятьсот";
-            }
-            else if (sum2 / 100 == 6)
-            {
-                hundred = "шестьсот";
-            }
-            else if (sum2 / 100 == 7)
-            {
-                hundred = "семьсот";
-            }
-            else if (sum2 / 100 == 8)
-            {
-                hundred = "восемьсот";
-            }
-            else if (sum2 / 100 == 9)
-            {
-                hundred = "девятьсот";
-            }
-            else
-            {
-                hundred = "";
-            }
-
-            int sum3 = sum % 100;
-            int sum4 = sum3 % 10;
-
-            string ten = "";
-            if (sum3 / 10 == 1)
-            {
-                if (sum4 == 0) ten = "десять";
-                else if (sum4 == 1) ten = "одиннадцать";
-                else if (sum4 == 2) ten = "двенадцать";
-                else if (sum4 == 3) ten = "тринадцать";
-                else if (sum4 == 4) ten = "четырнадцать";
-                else if (sum4 == 5) ten = "пятнадцать";
-                else if (sum4 == 6) ten = "шестнадцать";
-                else if (sum4 == 7) ten = "семнадцать";
-                else if (sum4 == 8) ten = "восемнадцать";
-                else if (sum4 == 9) ten = "девятнадцать";
-            }
-            else if (sum3 / 10 == 2)
-            {
-                ten = "двадцать";
-            }
-            else if (sum3 / 10 == 3)
-            {
-                ten = "тридцать";
-            }
-            else if (sum3 / 10 == 4)
-            {
-                ten = "сорок";
-            }
-            else if (sum3 / 10 == 5)
-            {
-                ten = "пятьдесят";
-            }
-            else if (sum3 / 10 == 6)
-            {
-                ten = "шестьдесят";
-            }
-            else if (sum3 / 10 == 7)
-            {
-                ten = "семьдесят";
-            }
-            else if (sum3 / 10 == 8)
-            {
-                ten = "восемьдесят";
-            }
-            else if (sum3 / 10 == 9)
-            {
-                ten = "девяносто";
-            }
-
-
-            string one;
-            if (ten == "одиннадцать" || ten == "двенадцать" || ten == "тринадцать" ||
-                ten == "четырнадцать" || ten == "пятнадцать" || ten == "шестнадцать" ||
-                ten == "семнадцать" || ten == "восемнадцать" || ten == "девятнадцать")
-            {
-                one = "";
-            }
-            else if (sum4 == 1)
-            {
-                one = "один";
-            }
-            else if (sum4 == 2)
-            {
-                one = "два";
-            }
-            else if (sum4 == 3)
-            {
-                one = "три";
-            }
-            else if (sum4 == 4)
-            {
-                one = "четыре";
-            }
-            else if (sum4 == 5)
-            {
-                one = "пять";
-            }
-            else if (sum4 == 6)
-            {
-                one = "шесть";
-            }
-            else if (sum4 == 7)
-            {
-                one = "семь";
-            }
-            else if (sum4 == 8)
-            {
-                one = "восемь";
-            }
-            else if (sum4 == 9)
-            {
-                one = "девять";
-            }
-            else
-            {
-                one = "";
-            }
-
-
-            string dollar;
-            if (sum3 / 10 == 1)
-            {
-                dollar = "гривень";
-            }
-            else if (sum3 / 10 != 1 && sum4 == 1)
-            {
-                dollar = "гривна";
-            }
-            else if (sum3 / 10 != 1 && sum4 == 0)
-            {
-                dollar = "гривень";
-            }
-            else if (sum3 / 10 != 1 && sum4 > 1 && sum4 <= 4)
-            {
-                dollar = "гривень";
-            }
-            else
-            {
-                dollar = "гривень";
-            }
+            Console.WriteLine($"количество четных цифр в числе {number} равно: {count}");
 
 
 
-            if (mistake == true)
+            //2
+            ulong sumarry = 0;
+            int a;
+            int b;
+            Console.WriteLine("введите число а (англискую): ");
+            while (true)
+                if (!int.TryParse((Console.ReadLine()), out a))
+                {
+                    Console.WriteLine("неккоректный ввод | введите целое число а");
+                }
+                else
+                {
+                    break;
+                }
+            Console.WriteLine("введите число б (англискую): ");
+            while (true)
             {
-                Console.WriteLine($"выдача: {thousand} {hundred} {ten} {one} {dollar}");
+                if (!int.TryParse((Console.ReadLine()), out b) || a > b)
+                {
+                    Console.WriteLine("неккоректный ввод | введите целое число б. Также б должно быть больше а");
+                }
+                else
+                {
+                    break;
+                }
             }
-            else
+            for (int i = a; i <= b; i++)
             {
-                Console.WriteLine("сумма должна быть от 1 до 9999");
+                if (i % 2 != 0)
+                {
+                    sumarry += (ulong)(i * i);
+                }
             }
+            Console.WriteLine($"сумма квадратов целых нечетных чисел от а до б равна: {sumarry}");
+
+
+
+            //3
+            Console.WriteLine("введите пароль: ");
+            const int correctpassword = 12345;
+            int password;
+            int attempts = 5;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out password) && password == correctpassword)
+                {
+                    Console.WriteLine("дарова");
+                    return;
+                }
+                else
+                {
+                    if (attempts > 1)
+                    {
+                        attempts--;
+                        Console.WriteLine($"ошибка | пароль неверный {attempts}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("лимит | больше нельзя");
+                        return;
+                    }
+                }
+            }
+
+
         }
     }
 }
