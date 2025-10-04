7baa73e9f7c9c658ec85c316ca037ca7ad6a322f
llProtikll <dimacornuta@gmail.com>
2025-10-04 23:12:12 +0300

    dz7

--- Dzz/Program.cs
+++ Dzz/Program.cs
@@ -1,102 +1,128 @@
-﻿using System.Collections.Generic;
-
-namespace Dzz
+﻿namespace Dzz
 {
     internal class Program
     {
         static void Main(string[] args)
-        {   
+        {
 
             //1
 
-            int[] numbers = new int[10];
-            for (int i = 0; i < numbers.Length; i++)
+            Console.WriteLine("1");
+            int[,] matrix = new int[3, 5];
+            Console.WriteLine("введите элементы матрицы:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                numbers[i] = Random.Shared.Next(7, 15);
-            }
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                if (numbers[i] > 10)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    numbers[i] = numbers[i] - 10;
-                }
-            }
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.Write($"{numbers[i]} ");
-            }
-
-            //2
-            int[] numbers1 = Array.ConvertAll(new int[20],
-                 _ => Random.Shared.Next(10, 31));
-
-            int[] numbers2 = Array.ConvertAll(new int[20],
-              _ => Random.Shared.Next(10, 31));
-
-            int[] numbers3 = new int[20];
-            Console.Write("\nсуммы соответствующих элементов 2 массивов: ");
-            for (int i = 0; i < numbers3.Length; i++)
-            {
-                numbers3[i] = numbers1[i] + numbers2[i];
-            }
-            for (int i = 0; i < numbers3.Length; i++)
-            {
-                Console.Write($"{numbers3[i]} ");
-            }
-            double average = 0;
-            for (int i = 0; i < numbers3.Length; i++)
-            {
-                average += (double)numbers3[i] / 2;
-            }
-
-            Console.WriteLine($"\nсреднее арифметическое элементов 3 массива: {average}");
-
-            int MinValue = numbers3[0];
-            int MaxValue = numbers3[0];
-            for (int i = 0; i < numbers3.Length; i++)
-            {
-                if (numbers3[i] < MinValue)
-                {
-                    MinValue = numbers3[i];
-                }
-                if (numbers3[i] > MaxValue)
-                {
-                    MaxValue = numbers3[i];
-                }
-            }
-
-            Console.WriteLine($"минимальное значение 3 массива: {MinValue}");
-            Console.WriteLine($"максимальное значение 3 массива: {MaxValue}");
-
-            //3 
-            int[] numbers4 = Array.ConvertAll(new int[20],
-                _ => Random.Shared.Next(0, 51));
-            Console.Write("элементы неотсортированного 4 массива: ");
-            for (int i = 0; i < numbers4.Length; i++)
-            {
-                Console.Write($"{numbers4[i]} ");
-            }
-
-            for (int i = 0; i < numbers4.Length - 1; i++)
-            {
-                bool swapped = false;
-                for (int j = 0; j < numbers4.Length - i - 1; j++) 
-                {
-                    if (numbers4[j] < numbers4[j + 1])
+                    while (!int.TryParse(Console.ReadLine(), out matrix[i, j]))
                     {
-                        int temp = numbers4[j];
-                        numbers4[j] = numbers4[j + 1];
-                        numbers4[j + 1] = temp;
-                        swapped = true;
+                        Console.WriteLine("ошибка | введите целое число");
                     }
                 }
-                if (!swapped) break; 
+            }
+            Console.WriteLine("визуал матрицы:");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            int sum = 0;
+            for (int i = 1; i == 1 || i == 2; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+            Console.WriteLine($"сумма элементов 2 и 3 строк матрицы равна: {sum}");
+            
+            //2
+
+            Console.WriteLine("2");
+            int[,] matrix2 = new int[4, 4];
+            for (int i = 0; i < matrix2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    matrix2[i, j] = Random.Shared.Next(10, 101);
+                }
             }
 
-            Console.Write("элементы отсортированного 4 массива: ");
-            for (int i = 0; i < numbers4.Length; i++)
+            Console.WriteLine($"элементы матрицы равны: ");
+            for (int i = 0; i < matrix2.GetLength(0); i++)
             {
-                Console.Write($"{numbers4[i]} ");
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix2[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            int[] arraysumline = new int[matrix2.GetLength(0)];
+            for (int i = 0; i < matrix2.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix2.GetLength(1); j++)
+                {
+                    arraysumline[i] += matrix2[i, j];
+                }
+            }
+
+            int maxValue = arraysumline[0];
+            int maxIndex = 0;
+            for (int i = 0; i < matrix2.GetLength(0); i++)
+            {
+                if (arraysumline[i] > maxValue)
+                {
+                    maxValue = arraysumline[i];
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine($"наибольшая сумма строки матрицы равна - {maxValue} ном строки {maxIndex + 1}");
+            
+            //3
+
+            Console.WriteLine("3");
+            int[,] matrix3 = new int[4, 4];
+            for (int i = 0; i < matrix3.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix3.GetLength(1); j++)
+                {
+                    matrix3[i, j] = Random.Shared.Next(-100, 101);
+                }
+            }
+
+            Console.WriteLine("элементы матрицы равны: ");
+            for (int i = 0; i < matrix3.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix3.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix3[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("положительные элементы главной диагонали матрицы: ");
+            
+            for (int i = 0; i < matrix3.GetLength(0); i++)
+            {
+                if (matrix3[i, i] >= 0)
+                {
+                    Console.Write($"{matrix3[i, i]}\t");
+                }
+            }
+
+            Console.WriteLine("\nположительные элементы второстепенной диагонали матрицы: ");
+
+            for (int i = 0; i < matrix3.GetLength(0); i++)
+            {
+                if (matrix3[i, matrix3.GetLength(1) - i - 1] > 0)
+                {
+                    Console.Write($"{matrix3[i, matrix3.GetLength(1) - i - 1]}\t");
+                }
             }
         }
     }
