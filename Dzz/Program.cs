c4cd9397904347b56aee697ba44626d18f8f9fd1
llProtikll <dimacornuta@gmail.com>
2025-10-04 22:24:06 +0300

    Добавьте файлы проекта.

---
+++ Dzz/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Dzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("годиков:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("рост:");
+            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("гендер (м/ж):");
+            string gender = Console.ReadLine();
+            Console.WriteLine("буква:");
+            char letter = char.Parse(Console.ReadLine());
+            Console.WriteLine("город:");
+            string city = Console.ReadLine();
+
+            Console.WriteLine($"имя: {name}");
+            Console.WriteLine($"годиков: {age}");
+            Console.WriteLine($"рост: {height}");
+            Console.WriteLine($"гендер (м/ж): {gender}");
+            Console.WriteLine($"буква: {letter}");
+            Console.WriteLine($"город: {city}");
+        }
+    }
+}
